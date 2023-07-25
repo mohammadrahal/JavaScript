2da0b491edfdaab55a0834804722ea9e6271d217
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-07-25 14:40:52 +0300

    Done with task 3

---
+++ task_3/js/app.js
@@ -0,0 +1,19 @@
+const num1= 1;
+const num2 = 3;
+
+if(num1 > num2){
+    console.log("hello");
+} else{
+    console.log("bye");
+}
+
+// boolean
+
+const canPass = false;
+
+if(canPass == 0){
+    console.log("enter");
+}else {
+    console.log("try again");
+}
+

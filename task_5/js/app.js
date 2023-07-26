00e15ea9e29740f91fba476f12dc2be97a26d184
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-07-26 12:32:40 +0300

    Done task 5

---
+++ task_5/js/app.js
@@ -0,0 +1,11 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+for (let i = 0; i < array.length; i++) {
+
+    if (i % 2 == 0) {
+        console.log(i + "" + "is even")
+    } else {
+        console.log(i + "" + "is odd")
+    }
+
+}
